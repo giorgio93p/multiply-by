90f88d9594e5f3b451d43a127f4cd6550495afe7
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2020-01-24 11:34:59 +0200

    I18n

--- multiply-by.py
+++ multiply-by.py
@@ -3,6 +3,6 @@ def render(table, params):
     for column in params['columns']:
         table[column] = params['constant'] * table[column]
     if params['constant'] < 0:
-        return (table, "The number is negative")
+        return (table, ("is_negative", {}, {"module": "multiply-by"}))
     else:
         return table
