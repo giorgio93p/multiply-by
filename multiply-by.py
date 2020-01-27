e8406cbd060bb175437877786a6b37f420858d8e
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2020-01-27 17:33:17 +0200

    Fix trans invocation

--- multiply-by.py
+++ multiply-by.py
@@ -1,9 +1,9 @@
-from cjwmodule.i18n import trans
+from cjwmodule.i18n.funcs import trans
 
 def render(table, params):
     for column in params['columns']:
         table[column] = params['constant'] * table[column]
     if params['constant'] < 0:
-        return (table, trans("is_negative"))
+        return (table, trans("is_negative", "The number is negative"))
     else:
         return table
